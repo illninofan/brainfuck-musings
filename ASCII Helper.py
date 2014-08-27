fde3d94dcacfff46709be3df6f0fce3505af40b8
Alex Garland <alex.d.garland@gmail.com>
2014-08-27 19:00:57 +0100

    Create ASCII Helper.py

---
+++ ASCII Helper.py
@@ -0,0 +1,22 @@
+for char in 'Brainfuck is CRAZY!':
+    print("# '{0}' has ASCII code {1}".format(char, ord(char)))
+    
+# 'B' has ASCII code 66
+# 'r' has ASCII code 114
+# 'a' has ASCII code 97
+# 'i' has ASCII code 105
+# 'n' has ASCII code 110
+# 'f' has ASCII code 102
+# 'u' has ASCII code 117
+# 'c' has ASCII code 99
+# 'k' has ASCII code 107
+# ' ' has ASCII code 32
+# 'i' has ASCII code 105
+# 's' has ASCII code 115
+# ' ' has ASCII code 32
+# 'C' has ASCII code 67
+# 'R' has ASCII code 82
+# 'A' has ASCII code 65
+# 'Z' has ASCII code 90
+# 'Y' has ASCII code 89
+# '!' has ASCII code 33
